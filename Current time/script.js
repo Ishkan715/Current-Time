c5c8db0deeee42dabff6829fe210b351a91500da
Ishkan Oganesyan <62455773+Ishkan715@users.noreply.github.com>
2021-09-12 22:09:57 -0700

    Add files via upload

---
+++ Current time/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth() + 1;
+const day = date.getDate();
+const hour = date.getHours();
+const min = date.getMinutes();
+const sec = date.getSeconds();
+
+function getDate(date) {}
